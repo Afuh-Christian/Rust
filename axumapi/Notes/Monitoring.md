387a0a796a9a81f4c663791b2981c66f83b2cac6
Afuh-Christian <afuhchristian10@gmail.com>
2025-05-31 19:09:55 +0100

    feat: Implemented Prometheus metrics endpoint
    This commit introduces Prometheus metrics to the axumapi application.
    
    - Added the `axum-prometheus` crate as a dependency.
    - Implemented a `/metrics` endpoint that exposes Prometheus metrics.
    - Added a `PrometheusMetricLayer` to the router to collect metrics for all routes.

---
+++ axumapi/Notes/Monitoring.md
@@ -0,0 +1,9 @@
+# Monitoring with Grafana and Prometheus
+
+ [Prometheus Docs](https://prometheus.io/)
+ [Grafana Docs](https://grafana.com/)
+
+```sh
+cargo add axum-prometheus
+
+```
--- axumapi/src/main.rs
+++ axumapi/src/main.rs
@@ -1,12 +1,11 @@
-use axum::{ middleware, routing::get, Extension, Router
-};
+use axum::{ middleware, routing::get, Extension, Router };
+use axum_prometheus::{PrometheusMetricLayer};
 use sea_orm::{  Database, DatabaseConnection};
 
 mod models;
 mod routes;
 mod handlers;
 mod utils;
-
 
 #[tokio::main]
 async fn main() {
@@ -15,22 +14,21 @@ async fn main() {
     // connect database ... 
     let db_url = (*utils::constants::DATABASE_URL).clone();
     let db: DatabaseConnection = Database::connect(db_url).await.expect("Failed to connect to db");
-
+    let (prometheus_layer, metric_handle) = PrometheusMetricLayer::pair();
+    
     // routes .. 
     let app = Router::new()
-    .route("/", get(|| async { "Hello, World!" }))
+    .route("/", get(|| async {"Hello, World!"}))
     .merge(routes::user_routes::user_routes())
     .merge(routes::home_routes::home_routes())
     .route_layer(middleware::from_fn(utils::guard::guard)) // Apply this middleware here so it only affects the routes above ...
     .merge(routes::auth_routes::auth_routes())
-    .layer(Extension(db.clone())); // database
+    .layer(Extension(db.clone())) // database
+    .route("/metrics", get(||async move {metric_handle.render()}))
+    .layer(prometheus_layer); // this layer will collect metrics for all routes
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
     
-
 }
-
-
-
