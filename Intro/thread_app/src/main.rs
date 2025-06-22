c6cce073da41812d8a477d45c6c1629b08672706
Afuh-Christian <afuhchristian10@gmail.com>
2025-06-22 20:34:25 +0100

    .

--- Intro/thread_app/src/main.rs
+++ Intro/thread_app/src/main.rs
@@ -1,11 +1,24 @@
+use std::sync::Mutex;
 use std::thread;
 
 fn main() {
-    let v = vec![1, 2, 3];
+    let counter = Mutex::new(0);
+    let mut handles = vec![];
+    // Create 10 threads that each increment the counter
+    for _ in 0..10 {
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+            *num += 1;
+        });
+        handles.push(handle);
+    }
 
-    let handle = thread::spawn(|| {
-        println!("Here's a vector: {v:?}");
-    });
+    for handle in handles {
+        handle.join().unwrap();
+    }
 
-    handle.join().unwrap();
+
+
+    println!("Result: {}", counter.lock().unwrap());
+
 }
